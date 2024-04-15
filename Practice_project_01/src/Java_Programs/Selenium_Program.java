244dcec5d5a7a3e77cd7627072351dad30f8228f
praj7073 <167002697+praj7073@users.noreply.github.com>
2024-04-15 16:27:52 +0530

    Update Selenium_Program.java

--- Practice_project_01/src/Java_Programs/Selenium_Program.java
+++ Practice_project_01/src/Java_Programs/Selenium_Program.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Selenium_Program {
 	public static void main(String args[]) {
 		WebDriver driver = new ChromeDriver();
-		// System.out.println("Hii");
 		driver.get("https://www.google.com");
 		driver.getTitle();
 		System.out.println(driver.getTitle());
